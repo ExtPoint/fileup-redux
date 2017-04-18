9cd68e51624f28af2eca59dac3cc994ee2e0c61c
Kozhin Vladimir <affka@affka.ru>
2017-04-18 22:41:14 +0700

    PropTypes fix

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import React, {PropTypes} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import FileUp from 'fileup-core';
 import File from 'fileup-core/lib/models/File';
